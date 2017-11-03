4b159d2fe5c6e01bdc53d063e568b6225fc87bde
Lucy Pieren <32195108+pierenl-BPC-Cyan@users.noreply.github.com>
2017-11-03 09:24:33 +0000

    Update starter.php

--- starter.php
+++ starter.php
@@ -1,12 +1,8 @@
-<HTML>
-
-<body>
-
 <?php
-echo "Lucy";
-echo "Pieren";
+$number = rand(1,6);
+if ($number = "1") {
+    echo "Almost Snake Eyes!";
+} elseif ($number = "6") {
+    echo "Winner!";
+}
 ?>
-
-</body>
-
-</HTML>
